7385b8e064cbc878b3978a40df2f91a3edfe1e54
terrancebrunner <terrancebrunner@gmail.com>
2025-11-29 14:30:22 -0500

    post first build

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  output: "export", // generate static HTML for all pages
 }
 
 export default nextConfig
